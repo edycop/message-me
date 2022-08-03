f4f6c54311aaaed9562b8b74a050392db15d2f2f
Edwin Caldon <edycop@gmail.com>
2022-08-03 12:16:12 -0500

    add flash messages to display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,13 @@
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+
+  // Flash messages close button
+  $('.message .close').on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
 })
