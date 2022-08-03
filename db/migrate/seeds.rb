5b83c642ec310d961e1c0487b7478b6e7d3605b2
Edwin Caldon <edycop@gmail.com>
2022-08-03 11:39:27 -0500

    add message model and associate it with user model

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -3,3 +3,6 @@
 
 User.create(username: 'userone', password: 'password123')
 User.create(username: 'usertwo', password: 'password123')
+
+Message.create(body: 'Hi, this is a new message', user: User.last)
+Message.create(body: 'This is the second message', user: User.last)
